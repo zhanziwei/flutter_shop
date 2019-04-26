f169443745e54531657acc5cb7f94a1c0509a460
zhanziwei <1715024080@qq.com>
2019-04-26 21:36:35 +0800

    打通底部导航栏

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'member_page.dart';
+import 'home_page.dart';
+import 'cart_page.dart';
+import 'category_page.dart';
 
-class IndexPage extends StatelessWidget {
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  int _currentIndex = 0;
+  var _currentPage;
+
+  final List<BottomNavigationBarItem> _bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: Text('分类')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: Text('购物车')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: Text('会员中心')
+    ),
+  ];
+
+  final List _pageTabs = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  @override
+  void initState() {
+    _currentPage = _pageTabs[_currentIndex];
+    super.initState();
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-     child: Scaffold(
-       appBar: AppBar(
-         title: Text("百姓生活+"),
-       ),
-       body: Center(
-         child: Text('百姓生活+'),
-       ),
-     ),
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomTabs,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _currentPage = _pageTabs[index];
+          });
+        },
+      ),
+      body: _currentPage,
     );
   }
 }
+
