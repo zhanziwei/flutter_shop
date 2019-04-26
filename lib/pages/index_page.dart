03adc52ccde681078b32b437caee154d2d8c94b4
zhanziwei <1715024080@qq.com>
2019-04-26 20:01:15 +0800

    编写入口文件

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     child: Scaffold(
+       appBar: AppBar(
+         title: Text("百姓生活+"),
+       ),
+       body: Center(
+         child: Text('百姓生活+'),
+       ),
+     ),
+    );
+  }
+}
